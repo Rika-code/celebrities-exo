a376474a6f34d4dd52bb240be0167677a0a7773c
Chloé <chloe.du.16@live.fr>
2023-11-23 12:08:50 +0100

    add error message

--- src/page/LikesPage.jsx
+++ src/page/LikesPage.jsx
@@ -2,14 +2,32 @@ import { useState } from "react";
 
 function LikesPage () {
     const [likescount, setLikescount] = useState(0);
+    const [messageDisplay, setMessageDisplay] = useState(true)
 
     const clickHandle = () => {
-        return setLikescount(Likescount + 1);
+        if (likescount < 5) {
+            setLikescount(likescount + 1)
+        } else {
+            setMessageDisplay(true)
+            setLikescount(0)
+        }
+        
     }
 
-    return (
-        <main>
-            <button onClick={clickHandle}>Like</button>
+    const handleCloseMessage = () => {
+         setMessageDisplay(false)
+         
+    }
+    return (   
+
+         <main>
+        {messageDisplay && (
+           <div>
+                <p>Vous ne pouvez pas liker plus de 5 fois</p>
+                <button onClick={handleCloseMessage}>Fermer le message</button> 
+                </div>
+           )};
+        <button onClick={clickHandle}>Like</button>
             <p> Vous avez liké {likescount} fois</p>
         </main>
     )
