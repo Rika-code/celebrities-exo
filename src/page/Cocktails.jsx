5f1d3f64e9463ab8bd1c819b92807a9462a1e2e3
Chloé <chloe.du.16@live.fr>
2023-11-23 15:20:51 +0100

    add fetch and usestate

--- src/page/Cocktails.jsx
+++ src/page/Cocktails.jsx
@@ -1,14 +1,24 @@
+import { useState } from "react";
 
 function Cocktails () {
+    const [cocktails, setCocktails] = useState(null); // je déclare mon state pour récuperer mon API
+
+    if (!cocktails) {                                                  // la boucle if permet que le rechargement des cocktails ne soit pas infini. 
     fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=") // Je déclare fetch qui va prendre en compte le lien de l'API
-    .then((response) => {  // Avec Response, je donne un temps d'attente pour que le fetch collecte les données de l'API
-        return response.json();
+    .then((cocktailsResponse) => {  // Ici, then récupère les données en attendant.
+        return cocktailsResponse.json(); // et là, il la convertie en json
     })
-    .then((json) => {
-        console.log(json); // Et enfin, je l'affiche a l'aide de mon console.log
-    })
+
+    .then((cocktailsInJs) => {  
+        console.log(cocktailsInJs); // J'affiche la conversion de mes données en Json a l'aide de mon console.log
+        setCocktails(cocktailsInJs) 
+    })}
+// Dans mon return ci-dessous, je vais vérifier si mes données sont chargées en les affichants.
     return (
-    <p>Je suis pas débutante, je suis professionnelle !!!!!</p>
+   <main> 
+    {cocktails ? <article> cocktails prêts avec la paille</article> : 
+    <p>chargement des cocktails...</p>}
+   </main>
     ) 
 }
 export default Cocktails
